e117ce8f93c09f6795d5667e87efb34fc126e4d5
Akhmedov76 <ahmedovj7686@gmail.com>
2025-04-22 11:05:45 +0500

    updapte admin.py

--- api/country/admin.py
+++ api/country/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from api.country.models import Region, District, Country
 
-admin.site.register(Region)
-admin.site.register(District)
-admin.site.register(Country)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+    search_fields = ('name', 'code')
+    ordering = ('name',)
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'country')
+    search_fields = ('name', 'code')
+    list_filter = ('country',)
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'region')
+    search_fields = ('name',)
+    ordering = ('name',)
