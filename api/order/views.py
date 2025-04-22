e821b33d2edadfc6c73b9f8416c15ee07b2a62c3
Akhmedov76 <ahmedovj7686@gmail.com>
2025-04-22 11:44:46 +0500

    create order view

--- api/order/views.py
+++ api/order/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from api.order.models import DeliveryRequest, DeliveryForDrivers
+from api.order.serializers import OrderSerializer, OrderCreateSerializer
+
+
+class DeliveryOrderView(ModelViewSet):
+    queryset = DeliveryRequest.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(role=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'customer':
+            return self.queryset.filter(role=user)
+        return self.queryset
+
+
+class OrderCreateView(ModelViewSet):
+    queryset = DeliveryForDrivers.objects.all()
+    serializer_class = OrderCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(role=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'customer':
+            return self.queryset.filter(role=user)
+        return self.queryset
