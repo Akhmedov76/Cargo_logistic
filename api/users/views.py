b5119ec182bacf168291699bb9e672b436797bb2
Akhmedov76 <ahmedovj7686@gmail.com>
2025-04-22 13:04:00 +0500

    update

--- api/users/views.py
+++ api/users/views.py
@@ -5,23 +5,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import action  # Correct import
-from django.contrib.auth import authenticate
+from rest_framework.decorators import action
+from django.contrib.auth import authenticate, login
 
 from api.users.models import User
 from api.users.serializers import UserSerializer, UserCreateSerializer, RegisterSerializer, LoginSerializer
-
-
-class UserListCreateView(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserCreateSerializer
-
-    @swagger_auto_schema(query_serializer=UserSerializer)
-    @action(detail=False, methods=['get'], url_path='get-users')
-    def get_users(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class RegisterView(APIView):
@@ -55,15 +43,19 @@ class LoginView(APIView):
                 password=serializer.validated_data['password']
             )
             if user:
+                login(request, user)
                 return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
             return Response({"error": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-"""{
-  "username": "jastin",
-  "password": "74108520Jas",
-  "email": "jas@example.com",
-  "phone_number": "+999815112",
-  "role": "operator"
-}"""
+class UserListCreateView(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+
+    @swagger_auto_schema(query_serializer=UserSerializer)
+    @action(detail=False, methods=['get'], url_path='get-users')
+    def get_users(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
