5d9a1dafd3201998b541ca753eeff824c9874703
Akhmedov76 <ahmedovj7686@gmail.com>
2025-04-22 11:17:00 +0500

    updata admin.py for order model

--- api/order/admin.py
+++ api/order/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from api.order.models import DeliveryRequest, DeliveryForDrivers
 
-admin.site.register(DeliveryRequest)
-admin.site.register(DeliveryForDrivers)
+
+@admin.register(DeliveryRequest)
+class DeliveryRequestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cargo', 'weight', 'capacity', 'when', 'loading', 'download', 'services', 'role')
+    list_filter = ('role', 'cargo')
+
+
+@admin.register(DeliveryForDrivers)
+class DeliveryForDriversAdmin(admin.ModelAdmin):
+    list_display = ('id', 'role', 'loading', 'vehicle', 'body_volume', 'where', 'where_to', 'company')
+    list_filter = ('role', 'company', 'loading', 'vehicle',)
