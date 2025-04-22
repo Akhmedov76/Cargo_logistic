69c681372f277f5c320dd928bf99d69cf84e5059
Akhmedov76 <ahmedovj7686@gmail.com>
2025-04-22 07:14:04 +0500

    update order models.py

--- api/order/models.py
+++ api/order/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from api.base.mixins import TimeModelMixin
+from api.country.models import District
+from api.users.models import User
 
 
 class DeliveryRequest(TimeModelMixin, models.Model):
@@ -11,20 +13,20 @@ class DeliveryRequest(TimeModelMixin, models.Model):
         ('no load', 'No load')
     ]
     cargo = models.ForeignKey('CargoElements', on_delete=models.CASCADE, blank=True, null=True)
-    weight = models.DecimalField(max_digits=12, decimal_places=2, null=True)
-    capacity = models.DecimalField(max_digits=12, decimal_places=2, null=True)
+    weight = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    capacity = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
 
     when = models.CharField(max_length=255, blank=True, choices=when_loading)
-    loading = models.ForeignKey('District', on_delete=models.CASCADE, null=True,
+    loading = models.ForeignKey(District, on_delete=models.CASCADE, null=True,
                                 related_name='deliveryrequest_loading')
-    download = models.ForeignKey('District', on_delete=models.CASCADE, null=True,
+    download = models.ForeignKey(District, on_delete=models.CASCADE, null=True,
                                  related_name='deliveryrequest_download')
-    services = models.ForeignKey('ServicesModel', on_delete=models.CASCADE, blank=True)
-    role = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
+    services = models.ForeignKey('ServicesModel', on_delete=models.CASCADE, blank=True, null=True)
+    role = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = 'Delivery'
-        verbose_name_plural = 'Delivery'
+        verbose_name_plural = 'Deliveries'
 
 
 class DeliveryForDrivers(TimeModelMixin, models.Model):
@@ -36,18 +38,17 @@ class DeliveryForDrivers(TimeModelMixin, models.Model):
         ('with the removal of crossbars', 'With the removal of crossbars'),
         ('electricity', 'electricity')
     ]
-    role = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
+    role = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     loading = models.CharField(choices=Loading_choice, max_length=255, blank=True, null=True)
     vehicle = models.CharField(_('Vehicle'), max_length=55, blank=True, null=True)
-    body_volume = models.DecimalField(_('Volume'), max_digits=12, decimal_places=2, null=True)
-    where = models.ForeignKey('District', on_delete=models.CASCADE, null=True,
-                              )
-    where_to = models.ForeignKey('District', on_delete=models.CASCADE, null=True,
+    body_volume = models.DecimalField(_('Volume'), max_digits=12, decimal_places=2, null=True, blank=True)
+    where = models.ForeignKey(District, on_delete=models.CASCADE, null=True)
+    where_to = models.ForeignKey(District, on_delete=models.CASCADE, null=True,
                                  related_name='delivery_where_to')
     company = models.CharField(_('company'), max_length=55, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.loading}"
+        return self.loading if self.loading else "No loading choice"
 
     class Meta:
         verbose_name = 'Driver'
