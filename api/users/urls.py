152c4a059c1ceffde744c2d52c9cddac08b9fa75
Akhmedov76 <ahmedovj7686@gmail.com>
2025-04-22 17:11:37 +0500

    add token refresh

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api.users.views import UserListCreateView, RegisterView, LoginView
 
@@ -9,6 +10,8 @@ router.register(r'users', UserListCreateView, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/register/', RegisterView.as_view(), name='register'),
-    path('auth/login/', LoginView.as_view(), name='login')
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
