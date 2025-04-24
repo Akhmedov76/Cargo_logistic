a2146d384518ee573256fef303a93ea73d1ab8a5
Akhmedov76 <ahmedovj7686@gmail.com>
2025-04-24 14:54:39 +0500

    update name

--- api/order/urls.py
+++ api/order/urls.py
@@ -5,7 +5,7 @@ from api.order.views import CargoRequestView, DeliveryOrderView
 
 router = SimpleRouter()
 
-router.register(r'cargos', CargoRequestView, basename='delivery')
+router.register(r'cargo', CargoRequestView, basename='delivery')
 router.register(r'driver', DeliveryOrderView, basename='driver')
 
 urlpatterns = [
