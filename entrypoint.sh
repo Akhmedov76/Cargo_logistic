2e8e60661d6cd7a1ee3ede053c3dd3c5634a8829
Jasurbek Ahmedov <ahmedovj7686@gmail.com>
2025-10-01 18:07:45 +0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-# entrypoint.sh
+
+# DB tayyor bo'lishini kutish
+echo "Waiting for Postgres..."
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 1
+done
+echo "Postgres is up - continuing..."
 
 # Django migratsiyalarini bajarish
-echo "Applying database migrations..."
+echo "Applying migrations..."
 python manage.py migrate --noinput
 
-# Static fayllarni collect qilish (agar kerak bo'lsa)
+# Static fayllarni collect qilish
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
