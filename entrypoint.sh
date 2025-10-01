07f373d674fb27c267ee64db72419b0a22fc2d04
Jasurbek Ahmedov <ahmedovj7686@gmail.com>
2025-10-01 18:09:43 +0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,11 @@
 
 # DB tayyor bo'lishini kutish
 echo "Waiting for Postgres..."
-while ! nc -z $DB_HOST $DB_PORT; do
+while ! (echo > /dev/tcp/$DB_HOST/$DB_PORT) 2>/dev/null; do
   sleep 1
 done
 echo "Postgres is up - continuing..."
 
-# Django migratsiyalarini bajarish
-echo "Applying migrations..."
 python manage.py migrate --noinput
-
-# Static fayllarni collect qilish
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-# Daphne serverini ishga tushirish
-echo "Starting Daphne server..."
 daphne conf.asgi:application -b 0.0.0.0 -p 7009
